3bf9b908d26cff99dea84f824a534083fa04b30d
Friedonion <rkdwnd333@naver.com>
2025-04-02 22:05:17 +0900

    라이트 아이콘 추가

--- W04Plus/Week04+/Engine/Source/Editor/PropertyEditor/ControlEditorPanel.cpp
+++ W04Plus/Week04+/Engine/Source/Editor/PropertyEditor/ControlEditorPanel.cpp
@@ -283,8 +283,8 @@ void ControlEditorPanel::CreateModifyButton(ImVec2 ButtonSize, ImFont* IconFont)
                     SpawnedActor = World->SpawnActor<AActor>();
                     SpawnedActor->SetActorLabel(TEXT("OBJ_SpotLight"));
                     SpawnedActor->AddComponent<ULightComponentBase>();
-                    UBillboardComponent* Billboard = SpawnedActor->AddComponent<UBillboardComponent>();
-                    Billboard->SetTexture(L"Assets/Texture/T_SpotLight.png");
+                   /* UBillboardComponent* Billboard = SpawnedActor->AddComponent<UBillboardComponent>();
+                    Billboard->SetTexture(L"Assets/Texture/T_SpotLight.png");*/
                     break;
                 }
                 case OBJ_PARTICLE:
